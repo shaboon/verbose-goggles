777648cbbd8d4d563ed266aa0842c1dc710ecaa5
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-05-10 22:35:55 -0400

    more nav styling added

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,46 +2,53 @@ import React from "react";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange("Home")}
-          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#projects"
-          onClick={() => handlePageChange("Projects")}
-          className={
-            currentPage === "Projects" ? "nav-link active" : "nav-link"
-          }
-        >
-          Projects
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className="row bg-dark text-light">
+      <h1 className="justify-content-start col-8 px-5 nav-text">
+        My Portfolio
+      </h1>
+      <ul className="nav nav-tabs col-4 justify-content-end">
+        <li className="nav-item py-0">
+          <a
+            href="#home"
+            onClick={() => handlePageChange("Home")}
+            className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+          >
+            Home
+          </a>
+        </li>
+        <li className="nav-item py-0">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item py-0">
+          <a
+            href="#projects"
+            onClick={() => handlePageChange("Projects")}
+            className={
+              currentPage === "Projects" ? "nav-link active" : "nav-link"
+            }
+          >
+            Projects
+          </a>
+        </li>
+        <li className="nav-item py-0">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,9 @@ import "../css/styles.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to my portfolio!</p>
+    <div className="container mx-auto p-5 rounded home">
+      <h1 className="my-2">Home Page</h1>
+      <p className="my-3">Welcome to my portfolio!</p>
       <p>
         You'll find my projects, contact information, and more in the tabs
         above!
