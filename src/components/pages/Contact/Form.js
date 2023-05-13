32962d82ef2fe3f9526c50aee3eff4b59dd2964e
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-05-13 16:00:49 -0400

    portfolio tab completed

---
+++ src/components/pages/Contact/Form.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+export default function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [badName, setBadName] = useState(false);
+  const [badEmail, setBadEmail] = useState(false);
+  const [badMessage, setBadMessage] = useState(false);
+
+  const [formSubmit, setFormSubmit] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    setBadName(false);
+    setBadEmail(false);
+    setBadMessage(false);
+
+    if (name === "") {
+      setBadName(true);
+      alert("Please enter your name");
+      return;
+    }
+    const emailContent = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailContent.test(email)) {
+      setBadEmail(true);
+      alert("Please enter a valid email address");
+      return;
+    }
+    if (message === "") {
+      setBadMessage(true);
+      alert("Please enter your message");
+      return;
+    }
+    setFormSubmit(true);
+
+    if (formSubmit) {
+      console.log("Name: " + name);
+      console.log("Email: " + email);
+      console.log("Message: " + message);
+    }
+  };
+
+  return (
+    <form className="container mx-auto my-5 col-12 p-3">
+      <input
+        className={`col-8 row mx-auto my-2 name form-control`}
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder={badName ? "Please enter your name" : "Name"}
+      />
+
+      <input
+        className={`col-8 row mx-auto my-2 email form-control ${
+          badEmail ? "is-invalid" : ""
+        }`}
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder={badEmail ? "Please enter a valid email address" : "Email"}
+      />
+
+      <textarea
+        className={`col-8 row mx-auto my-2 message form-control ${
+          badMessage ? "is-invalid" : ""
+        }`}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder={badMessage ? "Please enter your message" : "Message"}
+      />
+
+      <button
+        onClick={handleFormSubmit}
+        className="col-12 col-md-2 mx-auto my-3 row justify-content-center align-items-center submit-btn"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,62 +1,162 @@
 import React from "react";
+import github from "./img/github.png";
+import website from "./img/world-wide-web.png";
+import cms from "./img/cmsrunning.png";
+import work from "./img/workday.png";
+import password from "./img/password-gen.png";
+import finis from "./img/finis.png";
+import marvel from "./img/marvel.png";
+import proj3 from "./img/NoImageAvailable.png";
 
 export default function Projects() {
   return (
-    <div className="container col-10 row justify-content-center mx-auto my-5 rendered">
+    <div className="container col-10 row justify-content-center mx-auto my-5 p-5 rendered">
       <h1 className="col-12 row justify-content-center">My Projects</h1>
-      <article className="col-12 col-md-4">
-        <h2 className="row justify-content-center text-align-center">
-          Project 4
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          InterStellar Index
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={cms} alt="cms menu"></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">
+            Employee Tracker: CMS
+          </section>
+          <a
+            className="col-3 justify-content-center text-align-center"
+            href="https://github.com/shaboon/silver-palm-tree"
+          >
+            <img
+              src={github}
+              className="link-icon"
+              alt="Github Logo and Link"
+            />
+          </a>
+        </div>
       </article>
-      <article className="col-12 col-md-4">
-        <h2 className="row justify-content-center text-align-center">
-          Project 5
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          InterStellar Index
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={work}></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">
+            Work Day Scheduler
+          </section>
+          <a
+            className="col-1"
+            href="https://github.com/shaboon/potential-guacamole"
+          >
+            <img
+              src={github}
+              className="link-icon mx-1"
+              alt="Github Logo and Link"
+            />
+          </a>
+          <a
+            className="col-1"
+            href="https://shaboon.github.io/potential-guacamole/"
+          >
+            <img
+              src={website}
+              className="link-icon"
+              alt="Link to Deployed Website"
+            />
+          </a>
+        </div>
       </article>
-      <article className="col-12 col-md-4">
-        <h2 className="row justify-content-center text-align-center">
-          Project 6
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          InterStellar Index
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={password}></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">
+            Password Generator
+          </section>
+          <a
+            className="col-1"
+            href="https://github.com/shaboon/expert-adventure-builtido"
+          >
+            <img
+              src={github}
+              className="link-icon mx-1"
+              alt="Github Logo and Link"
+            />
+          </a>
+          <a
+            className="col-1"
+            href="https://shaboon.github.io/expert-adventure-builtido/"
+          >
+            <img
+              src={website}
+              className="link-icon"
+              alt="Link to Deployed Website"
+            />
+          </a>
+        </div>
       </article>
-      <article className="col-12 col-md-4">
-        <h2 className="row justify-content-center text-align-center">
-          Project 1
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          Isle Finis Noctis
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={finis}></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">
+            Isle Finis Noctis
+          </section>
+          <a
+            className="col-1"
+            href="https://github.com/shaboon/improved-happiness"
+          >
+            <img
+              src={github}
+              className="link-icon mx-1"
+              alt="Github Logo and Link"
+            />
+          </a>
+          <a
+            className="col-1"
+            href="https://shaboon.github.io/improved-happiness/"
+          >
+            <img
+              src={website}
+              className="link-icon"
+              alt="Link to Deployed Website"
+            />
+          </a>
+        </div>
       </article>
-      <article className="col-12 col-md-4">
-        <h2 className="row  justify-content-center text-align-center">
-          Project 2
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          Marvel Team Database
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={marvel}></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">Marvel Database</section>
+          <a
+            className="col-1"
+            href="https://github.com/anthonylieu/marvel-team-builder"
+          >
+            <img
+              src={github}
+              className="link-icon mx-1"
+              alt="Github Logo and Link"
+            />
+          </a>
+          <a
+            className="col-1"
+            href="https://intense-savannah-08912.herokuapp.com/"
+          >
+            <img
+              src={website}
+              className="link-icon"
+              alt="Link to Deployed Website"
+            />
+          </a>
+        </div>
       </article>
-      <article className="col-12 col-md-4">
-        <h2 className="row justify-content-center text-align-center">
-          Project 3
-        </h2>
-        <section className="row justify-content-center text-align-center">
-          <img></img>
-          InterStellar Index (No Image Available Yet)
-        </section>
+      <article className="bg-secondary rounded col-12 col-md-3 mx-1 p-2 my-3">
+        <img className="mx-auto proj-img" src={proj3}></img>
+        <div className="container row">
+          <section className="col-9 text-align-center">
+            InterStellar Index (Not Ready Yet)
+          </section>
+          <a
+            className="col-3"
+            href="https://github.com/shaboon/symmetrical-robot"
+          >
+            <img
+              src={github}
+              className="link-icon mx-1"
+              alt="Github Logo and Link"
+            />
+          </a>
+        </div>
       </article>
     </div>
   );
