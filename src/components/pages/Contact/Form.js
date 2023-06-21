1269247c54943c5d323f303f993df799eade1934
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-06-20 22:51:36 -0400

    added my email to message area, but more work will need to be done

--- src/components/pages/Contact/Form.js
+++ src/components/pages/Contact/Form.js
@@ -69,7 +69,11 @@ export default function Form() {
         }`}
         value={message}
         onChange={(e) => setMessage(e.target.value)}
-        placeholder={badMessage ? "Please enter your message" : "Message"}
+        placeholder={
+          badMessage
+            ? "Please enter your message"
+            : "Your Message - or - You can also directly email me at shaboon3928@gmail.com"
+        }
       />
 
       <button
